04b295079e4cd7a4c96ac0b3bb5590722ba04a1a
rodrigo <rodrigolr_15@hotmail.com>
2020-05-24 17:09:55 -0300

    endpoint to create webhooks

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,19 @@ class EventsController < ApplicationController
 	end
 
 	def create
+		issue_prm = params[:issue]
+		event_prm = {
+			action: params[:event][:action],
+			sender_id: params[:sender][:id],
+			repository_id: params[:repository][:id]
+		}
 
+		event_service = CreateEventService.new(issue_prm, event_prm)
+
+		if event_service.save
+			render json: {}, status: :created
+		else
+			render json: {}, status: :unprocessable_entity
+		end
 	end
 end
---
+++ app/services/create_event_service.rb
@@ -0,0 +1,28 @@
+class CreateEventService
+
+	def initialize(issue_prm, event_prm)
+    	@issue = Issue.find_by(github_id: issue_prm[:id])
+		if @issue.nil?
+			@issue = Issue.new({
+				github_id: issue_prm[:id],
+				number: issue_prm[:number],
+      			title: issue_prm[:title],
+      			url: issue_prm[:url]
+			})
+		end
+
+    	@event = Event.new(event_prm)
+  	end
+
+  	def save
+  		ActiveRecord::Base.transaction do
+			@issue.save!
+			@event.issue_id = @issue.id
+			@event.save!
+			true
+  		rescue  ActiveRecord::RecordInvalid => e
+  			false
+  		end
+  	end
+
+end
